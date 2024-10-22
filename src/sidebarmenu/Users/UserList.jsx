3667cd11dc58b05a3ad2eae114c51c55c41b4ffd
ajoy11s <ajoycse0811@gmail.com>
2024-10-22 17:04:46 +0600

    commit change

--- src/sidebarmenu/Users/UserList.jsx
+++ src/sidebarmenu/Users/UserList.jsx
@@ -6,6 +6,24 @@ import { Link } from "react-router-dom";
 export default function UserList() {
 
     const [userslist, setUsersList] = useState([]);
+    const [selectedEmail, setSelectedEmail] = useState('');
+    const [error, setError] = useState('');
+    const [nameValue, setNameValue] = useState('');
+
+    const handEditleLinkClick = (email) => {
+        setSelectedEmail(email);
+        document.getElementById('my_modal_3').showModal();
+    };
+
+    const handleChange = (event) => {
+        setNameValue(event.target.value);
+    };
+
+    const handleUpdateDataButtonClick = (e) => {
+        if (nameValue.trim() === '') {
+            alert('Name field cannot be empty!');
+        }
+    };
 
     useEffect(() => {
         fetch(import.meta.env.VITE_GET_ALL_USERS)
@@ -75,7 +93,7 @@ export default function UserList() {
                                             : users.issystemadmin ? "SystemAdmin" : ""
                                 }</td>
                                 <th className="space-x-2">
-                                    <Link onClick={() => document.getElementById('my_modal_3').showModal()}> <i className="pi pi-pen-to-square" style={{ fontSize: '1.5rem' }}></i></Link>
+                                    <Link onClick={() => handEditleLinkClick(users.email)}> <i className="pi pi-pen-to-square" style={{ fontSize: '1.5rem' }}></i></Link>
                                     <Link> <i className="pi pi-trash" style={{ fontSize: '1.5rem' }}></i></Link>
                                     <Link> <i className="pi pi-info-circle" style={{ fontSize: '1.5rem' }}></i></Link>
                                 </th>
@@ -89,27 +107,25 @@ export default function UserList() {
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
 
             <dialog id="my_modal_3" className="modal">
-                <div className="modal-box w-3/4">
-                    <form method="dialog" className="my-4 w-3/4">
+                <div className="modal-box" style={{ width: '35%' }}>
+                    <form method="dialog" className="my-4 w-full">
                         {/* if there is a button in form, it will close the modal */}
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
-                    <div className="form-control py-2 w-3/4">
-                        <input type="text" name="name" id="name" placeholder="Please enter your name" className="input input-bordered" required />
+                    <div className="form-control py-2 w-full">
+                        <input type="text" onChange={handleChange} value={nameValue} name="name" id="name" placeholder="Please enter your name" className="input input-bordered" required />
                     </div>
-                    <div className="form-control py-2 w-3/4">
+                    <div className="form-control py-2 w-full">
                         <input type="text" name="address" id="address" placeholder="Please enter your address" className="input input-bordered" required />
                     </div>
-                    <div className="form-control py-2 w-3/4">
+                    <div className="form-control py-2 w-full">
                         <input type="tel" name="tel" id="tel" placeholder="Please enter your mobile no" className="input input-bordered" required />
                     </div>
-                    <div className="form-control mt-2 flex justify-center items-center">
-                        <button className="btn btn-warning w-1/2">Update</button>
+                    <div className="form-control mt-2 flex justify-center items-center w-full">
+                        <button className="btn btn-warning w-1/2" onClick={handleUpdateDataButtonClick}>Update</button>
                     </div>
                 </div>
             </dialog>
-
-
         </div>
     )
 }
